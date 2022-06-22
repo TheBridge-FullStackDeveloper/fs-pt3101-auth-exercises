392d5680698013cce144cf2f80a6d2c9a204e9c3
Brujitari <natimontesg2@mail.com>
2022-06-22 22:18:15 +0200

    feat: basic react form

---
+++ simple-auth-flow-front/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Form from "./components/Register"
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Holi</h1>
+      </header>
+      <Form />
+      <footer>
+        <h6>All rights reserved to Natalia Montes</h6>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
